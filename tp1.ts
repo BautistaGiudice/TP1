0f561b690d03e62dbacb98faf0ea869d6b58ca11
SergioPeck <sergiopeckerle2004@gmail.com>
2024-04-12 21:30:24 -0300

    Commit Sergio

--- tp1.ts
+++ tp1.ts
@@ -1,4 +1,8 @@
 //Comienzo del trabajo
+import * as rls from"readline-sync";
+import * as fs from "fs";
 
 let products:string[] = new Array();
 let prices:number[] = new Array();
+prices[0]=13;
+prices[1]=13;
