f9c5c02e870756d14105e7c4e1b1198806a9366f
crigca <crigca@hotmail.com>
2024-04-13 18:19:24 -0300

    He creado la funcion showProducts

--- tp1.ts
+++ tp1.ts
@@ -1,4 +1,16 @@
-//Comienzo del trabajo.
 
 let products:string[] = new Array();
 let prices:number[] = new Array();
+
+
+
+function showProducts():void{
+    console.log("Producto  Precio:");
+    for(let i:number = 0; i < products.length; i++){
+        console.log(`${products[i]}: $${prices[i].toFixed(2)}`);
+
+
+    }
+}
+
+showProducts();
